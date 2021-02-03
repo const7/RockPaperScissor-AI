76879a87fede89b5c8a920dda3f54330ba1d90be
const7 <19154386+const7@users.noreply.github.com>
2021-02-03 17:39:14 +0800

    Remove useless code and beautify the code

--- getData.py
+++ getData.py
@@ -1,9 +1,8 @@
-import numpy as np
 import cv2
 import os
 import sys
 
-PATH = os.getcwd()+'\\'
+PATH = os.getcwd() + "\\"
 cap = cv2.VideoCapture(0)
 
 label = sys.argv[1]
@@ -16,16 +15,16 @@ except FileExistsError:
     pass
 
 ct = int(sys.argv[2])
-maxCt = int(sys.argv[3])+1
+maxCt = int(sys.argv[3]) + 1
 print("Hit Space to Capture Image")
 
 while True:
     ret, frame = cap.read()
-    cv2.imshow('Get Data : '+label,frame[50:350,100:450])
-    if cv2.waitKey(1) & 0xFF == ord(' '):
-        cv2.imwrite(SAVE_PATH+'\\'+label+'{}.jpg'.format(ct),frame[50:350,100:450])
-        print(SAVE_PATH+'\\'+label+'{}.jpg Captured'.format(ct))
-        ct+=1
+    cv2.imshow("Get Data : " + label,frame[50:350, 100:450])
+    if cv2.waitKey(1) & 0xFF == ord(" "):
+        cv2.imwrite(SAVE_PATH + "\\" + label + "{}.jpg".format(ct), frame[50:350, 100:450])
+        print(SAVE_PATH + "\\" + label + "{}.jpg Captured".format(ct))
+        ct += 1
     if ct >= maxCt:
         break
 
--- play.py
+++ play.py
@@ -1,64 +1,63 @@
-from keras.models import model_from_json
-import numpy as np
-from skimage import io
 import cv2
 import random
+import numpy as np
+from keras.models import model_from_json
+
 
 def prepImg(pth):
-    return cv2.resize(pth,(300,300)).reshape(1,300,300,3)
+    return cv2.resize(pth, (300, 300)).reshape(1, 300, 300, 3)
 
-def updateScore(play,bplay,p,b):
-    winRule = {'rock':'scissor','scissor':'paper','paper':'rock'}
+def updateScore(play, bplay, p, b):
+    winRule = {"rock":"scissor", "scissor":"paper", "paper":"rock"}
     if play == bplay:
-        return p,b
+        return p, b
     elif bplay == winRule[play]:
-        return p+1,b
+        return p + 1, b
     else:
-        return p,b+1
+        return p, b + 1
 
-with open('model.json', 'r') as f:
+with open("model.json", "r") as f:
     loaded_model_json = f.read()
 loaded_model = model_from_json(loaded_model_json)
 loaded_model.load_weights("modelweights.h5")
 print("Loaded model from disk")
 
 
-shape_to_label = {'rock':np.array([1.,0.,0.]),'paper':np.array([0.,1.,0.]),'scissor':np.array([0.,0.,1.])}
-arr_to_shape = {np.argmax(shape_to_label[x]):x for x in shape_to_label.keys()}
+shape_to_label = {"rock": np.array([1.,0.,0.]),"paper": np.array([0.,1.,0.]),"scissor": np.array([0.,0.,1.])}
+arr_to_shape = {np.argmax(shape_to_label[x]): x for x in shape_to_label.keys()}
 
-options = ['rock','paper','scissor']
-winRule = {'rock':'scissor','scissor':'paper','paper':'rock'}
+options = ["rock", "paper", "scissor"]
 rounds = 0
 botScore = 0
 playerScore = 0
 
 cap = cv2.VideoCapture(0)
-ret,frame = cap.read()
-loaded_model.predict(prepImg(frame[50:350,100:400]))
+_, frame = cap.read()
+loaded_model.predict(prepImg(frame[50:350, 100:400]))
 
 NUM_ROUNDS = 3
 bplay = ""
 
 
 while True:
-    ret , frame = cap.read()
-    frame = frame = cv2.putText(frame,"Press Space to start",(160,200),cv2.FONT_HERSHEY_SIMPLEX,1,(250,250,0),2,cv2.LINE_AA)
-    cv2.imshow('Rock Paper Scissor',frame)
-    if cv2.waitKey(1) & 0xff == ord(' '):
+    _, frame = cap.read()
+    frame = cv2.putText(frame, "Press Space to start", (160, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (250, 250, 0), 2, cv2.LINE_AA)
+    cv2.imshow("Rock Paper Scissor", frame)
+    if cv2.waitKey(1) & 0xff == ord(" "):
         break
 
 for rounds in range(NUM_ROUNDS):
     pred = ""
     for i in range(90):
-        ret,frame = cap.read()
+        _, frame = cap.read()
     
         # Countdown    
         if i//20 < 3 :
-            frame = cv2.putText(frame,str(i//20+1),(320,100),cv2.FONT_HERSHEY_SIMPLEX,3,(250,250,0),2,cv2.LINE_AA)
+            frame = cv2.putText(frame, str(i//20+1), (320, 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (250, 250, 0), 2, cv2.LINE_AA)
 
         # Prediction
         elif i/20 < 3.5:
-            pred = arr_to_shape[np.argmax(loaded_model.predict(prepImg(frame[50:350,100:400])))]
+            pred = arr_to_shape[np.argmax(loaded_model.predict(prepImg(frame[50:350, 100:400])))]
         
         # Get Bots Move
         elif i/20 == 3.5:
@@ -71,11 +70,11 @@ for rounds in range(NUM_ROUNDS):
             break
 
         cv2.rectangle(frame, (100, 150), (300, 350), (255, 255, 255), 2)
-        frame = cv2.putText(frame,"Player : {}      Bot : {}".format(playerScore,botScore),(120,400),cv2.FONT_HERSHEY_SIMPLEX,1,(250,250,0),2,cv2.LINE_AA)
-        frame = cv2.putText(frame,pred,(150,140),cv2.FONT_HERSHEY_SIMPLEX,1,(250,250,0),2,cv2.LINE_AA)
-        frame = cv2.putText(frame,"Bot Played : {}".format(bplay),(300,140),cv2.FONT_HERSHEY_SIMPLEX,1,(250,250,0),2,cv2.LINE_AA)        
-        cv2.imshow('Rock Paper Scissor',frame)
-        if cv2.waitKey(1) & 0xff == ord('q'):
+        frame = cv2.putText(frame,"Player : {}      Bot : {}".format(playerScore, botScore), (120, 400), cv2.FONT_HERSHEY_SIMPLEX, 1, (250, 250, 0), 2, cv2.LINE_AA)
+        frame = cv2.putText(frame, pred, (150, 140), cv2.FONT_HERSHEY_SIMPLEX, 1, (250, 250, 0), 2, cv2.LINE_AA)
+        frame = cv2.putText(frame, "Bot Played : {}".format(bplay), (300, 140), cv2.FONT_HERSHEY_SIMPLEX, 1, (250, 250, 0), 2, cv2.LINE_AA)        
+        cv2.imshow("Rock Paper Scissor", frame)
+        if cv2.waitKey(1) & 0xff == ord("q"):
             break
 
 if playerScore > botScore:
@@ -86,12 +85,12 @@ else:
     winner = "Bot Won.."
     
 while True:
-    ret,frame = cap.read()
-    frame = cv2.putText(frame,winner,(230,150),cv2.FONT_HERSHEY_SIMPLEX,1,(250,250,0),2,cv2.LINE_AA)
-    frame = cv2.putText(frame,"Press q to quit",(190,200),cv2.FONT_HERSHEY_SIMPLEX,1,(250,250,0),2,cv2.LINE_AA)
-    frame = cv2.putText(frame,"Player : {}      Bot : {}".format(playerScore,botScore),(120,400),cv2.FONT_HERSHEY_SIMPLEX,1,(250,250,0),2,cv2.LINE_AA)
-    cv2.imshow('Rock Paper Scissor',frame)
-    if cv2.waitKey(1) & 0xff == ord('q'):
+    _, frame = cap.read()
+    frame = cv2.putText(frame, winner, (230, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (250, 250, 0), 2, cv2.LINE_AA)
+    frame = cv2.putText(frame, "Press q to quit", (190, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (250, 250, 0), 2, cv2.LINE_AA)
+    frame = cv2.putText(frame, "Player : {}      Bot : {}".format(playerScore, botScore), (120, 400), cv2.FONT_HERSHEY_SIMPLEX, 1, (250, 250, 0), 2, cv2.LINE_AA)
+    cv2.imshow("Rock Paper Scissor", frame)
+    if cv2.waitKey(1) & 0xff == ord("q"):
             break
 
 cap.release()
