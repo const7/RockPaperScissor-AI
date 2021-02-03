2e65b730b83ee6e8f073b1899ab8addb9f8ed646
const7 <19154386+const7@users.noreply.github.com>
2021-02-03 18:18:04 +0800

    Change to platform independent paths

--- getData.py
+++ getData.py
@@ -2,28 +2,24 @@ import cv2
 import os
 import sys
 
-PATH = os.getcwd() + "\\"
-cap = cv2.VideoCapture(0)
-
 label = sys.argv[1]
 
-SAVE_PATH = os.path.join(PATH, label)
+SAVE_PATH = os.path.join(os.getcwd(), "data", label)
 
-try:
-    os.mkdir(SAVE_PATH)
-except FileExistsError:
-    pass
+if not os.path.exists(SAVE_PATH):
+    os.makedirs(SAVE_PATH)
 
 ct = int(sys.argv[2])
 maxCt = int(sys.argv[3]) + 1
 print("Hit Space to Capture Image")
 
+cap = cv2.VideoCapture(0) 
 while True:
     ret, frame = cap.read()
     cv2.imshow("Get Data : " + label,frame[50:350, 100:450])
     if cv2.waitKey(1) & 0xFF == ord(" "):
-        cv2.imwrite(SAVE_PATH + "\\" + label + "{}.jpg".format(ct), frame[50:350, 100:450])
-        print(SAVE_PATH + "\\" + label + "{}.jpg Captured".format(ct))
+        cv2.imwrite(os.path.join(SAVE_PATH, "{}{}.jpg".format(label, ct)), frame[50:350, 100:450])
+        print("{}{}{}.jpg Captured".format(os.path.join(SAVE_PATH, "{}{}.jpg".format(label, ct)), label, ct))
         ct += 1
     if ct >= maxCt:
         break
