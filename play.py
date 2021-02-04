18c219576b83b84b0d05628950e0b67f2c5ad80b
const7 <19154386+const7@users.noreply.github.com>
2021-02-04 13:08:46 +0800

    Change model path

--- play.py
+++ play.py
@@ -1,8 +1,19 @@
+import os
 import cv2
 import random
 import numpy as np
 from keras.models import model_from_json
 
+
+MODEL_PATH = "model"
+MODEL_STRUCT = "model.json"
+MODEL_WEIGHT = "modelweight.h5"
+
+import tensorflow as tf
+config = tf.compat.v1.ConfigProto(gpu_options=tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=0.9))
+config.gpu_options.allow_growth = True
+session = tf.compat.v1.Session(config=config)
+tf.compat.v1.keras.backend.set_session(session)
 
 def prepImg(pth):
     return cv2.resize(pth, (300, 300)).reshape(1, 300, 300, 3)
@@ -16,12 +27,45 @@ def updateScore(play, bplay, p, b):
     else:
         return p, b + 1
 
-with open("model.json", "r") as f:
+with open(os.path.join(MODEL_PATH, MODEL_STRUCT), "r") as f:
     loaded_model_json = f.read()
 loaded_model = model_from_json(loaded_model_json)
-loaded_model.load_weights("modelweights.h5")
-print("Loaded model from disk")
+loaded_model.load_weights(os.path.join(MODEL_PATH, MODEL_WEIGHT))
+print("========= Loaded model from disk ==========")
 
+# DATA_PATH = "data"
+# def getData(dataset):
+#     shape_to_label = {"rock": np.array([1., 0., 0.]), "paper": np.array([0., 1., 0.]), "scissor": np.array([0., 0., 1.])}
+
+#     imgData = list()
+#     labels = list()
+
+#     for dr in os.listdir(os.path.join(DATA_PATH, dataset)):
+#         if dr not in ["rock", "paper", "scissor"]:
+#             continue
+#         lb = shape_to_label[dr]
+#         i = 0
+#         for pic in os.listdir(os.path.join(DATA_PATH, dataset, dr)):
+#             path = os.path.join(DATA_PATH, dataset, dr, pic)
+#             img = cv2.imread(path)
+#             imgData.append([img, lb])
+#             imgData.append([cv2.flip(img, 1), lb]) #horizontally flipped image
+#             # imgData.append([cv2.resize(img[50:250, 50:250], (300,300)), lb]) # zoom : crop in and resize
+#             i += 2
+#         print("{}: {}".format(dr, i))
+#     np.random.shuffle(imgData)
+
+#     # reshape data to model input
+#     imgData,labels = zip(*imgData)
+#     imgData = np.array(imgData)
+#     labels = np.array(labels)
+
+#     return imgData, labels
+# testX, testY = getData("test")
+# from keras.optimizers import Adam
+# loaded_model.compile(optimizer=Adam(), loss="categorical_crossentropy", metrics=["acc"])
+# loss_and_metrics = loaded_model.evaluate(testX, testY, batch_size=8)
+# print(loss_and_metrics)
 
 shape_to_label = {"rock": np.array([1.,0.,0.]),"paper": np.array([0.,1.,0.]),"scissor": np.array([0.,0.,1.])}
 arr_to_shape = {np.argmax(shape_to_label[x]): x for x in shape_to_label.keys()}
